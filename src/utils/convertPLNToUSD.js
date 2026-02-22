5a6f4df49a8029b52e6fcd1841bd801669ffdfc8
Dariusz Malinowski <dariusz.m.malinowski@gmail.com>
2026-02-22 12:08:43 +0100

    add first tests

--- src/utils/convertPLNToUSD.js
+++ src/utils/convertPLNToUSD.js
@@ -1,4 +1,18 @@
 export const convertPLNToUSD = (PLN) => {
+  // 1. Sprawdzamy czy wejście jest tekstem lub jest puste
+  if (typeof PLN === 'string' || typeof PLN === 'undefined') {
+    return NaN;
+  }
+
+  // 2. Sprawdzamy czy to na pewno liczba (np. czy nie przekazano tablicy)
+  if (typeof PLN !== 'number') {
+    return 'Error';
+  }
+
+  // 3. Sprawdzamy czy liczba nie jest ujemna (logika biznesowa)
+  if (PLN < 0) {
+    return '$0.00';
+  }
 
   const PLNtoUSD = PLN / 3.5;
   
@@ -8,4 +22,4 @@ export const convertPLNToUSD = (PLN) => {
   });
 
   return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
-}
+};
